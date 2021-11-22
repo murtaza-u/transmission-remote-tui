287ee2351fb895a06920a24e238f5d47460e03d5
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2021-11-22 23:02:40 +0530

    Stop transmission-daemon when 'Q' is pressed

--- tui/keys.go
+++ tui/keys.go
@@ -9,7 +9,7 @@ func setKeys(session *core.Session) {
     tui.torrents.setKeys(session)
     tui.layout.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
         switch event.Rune() {
-        case 'q':
+        case 'q', rune(tcell.KeyESC):
             currentWidget = "torrents"
             tui.pages.RemovePage("details")
             return nil
@@ -22,6 +22,17 @@ func setKeys(session *core.Session) {
         case 'k':
             row, col := tui.overview.widget.GetScrollOffset()
             tui.overview.widget.ScrollTo(row - 1, col)
+            return nil
+        }
+
+        return event
+    })
+
+    tui.app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+        switch event.Rune() {
+        case 'Q':
+            core.SendRequest("session-close", "1", core.Arguments{}, session)
+            tui.app.Stop()
             return nil
         }
 
