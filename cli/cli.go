a643f4ff59e57dd2479908c6e7356bc252172040
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2022-07-04 21:31:14 +0530

    support remote url RPC url can now be passed using --url flag. Defaults to http://localhost.

--- cli/cli.go
+++ cli/cli.go
@@ -2,10 +2,11 @@ package cli
 
 import "flag"
 
-func ParseArgs() (username, password string, port int) {
+func ParseArgs() (username, password, url string, port int) {
 	flag.IntVar(&port, "port", 9091, "rpc port")
 	flag.StringVar(&username, "username", "", "username")
 	flag.StringVar(&password, "password", "", "password")
+	flag.StringVar(&url, "url", "http://localhost", "rpc url")
 	flag.Parse()
 	return
 }
